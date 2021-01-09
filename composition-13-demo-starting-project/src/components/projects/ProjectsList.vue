744f015a1c01ce7204536c578681a1e585eaa85d
tugberkgoc <tugberkgoc@gmail.com>
2021-01-09 22:04:43 +0300

    Convertion from options api to composition api

---
+++ composition-13-demo-starting-project/src/components/projects/ProjectsList.vue
@@ -0,0 +1,86 @@
+<template>
+  <base-container v-if="user">
+    <h2>{{ user.fullName }}: Projects</h2>
+    <base-search
+      v-if="hasProjects"
+      @search="updateSearch"
+      :search-term="enteredSearchTerm"
+    ></base-search>
+    <ul v-if="hasProjects">
+      <project-item
+        v-for="prj in availableProjects"
+        :key="prj.id"
+        :title="prj.title"
+      ></project-item>
+    </ul>
+    <h3 v-else>No projects found.</h3>
+  </base-container>
+  <base-container v-else>
+    <h3>No user selected.</h3>
+  </base-container>
+</template>
+
+<script>
+import { ref, computed, watch, toRefs } from 'vue'
+import ProjectItem from './ProjectItem.vue'
+
+export default {
+  components: {
+    ProjectItem
+  },
+  props: ['user'],
+  setup (props) {
+    const enteredSearchTerm = ref('')
+    const activeSearchTerm = ref('')
+
+    const availableProjects = computed(() => {
+      if (activeSearchTerm.value) {
+        return props.user.projects.filter(prj =>
+          prj.title.includes(activeSearchTerm.value)
+        )
+      }
+      return props.user.projects
+    })
+
+    const hasProjects = computed(() => {
+      return props.user.projects && availableProjects.value.length > 0
+    })
+
+    watch(enteredSearchTerm, newVal => {
+      setTimeout(() => {
+        if (newVal === enteredSearchTerm.value) {
+          activeSearchTerm.value = newVal
+        }
+      }, 300)
+    })
+
+    // const propsWithRefs = toRefs(props);
+    // const user = propsWithRefs.user;
+
+    const { user } = toRefs(props)
+
+    watch(user, () => {
+      enteredSearchTerm.value = ''
+    })
+
+    const updateSearch = val => {
+      enteredSearchTerm.value = val
+    }
+
+    return {
+      hasProjects,
+      updateSearch,
+      enteredSearchTerm,
+      availableProjects
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ composition-13-demo-starting-project/src/components/users/UserList.vue
@@ -0,0 +1,106 @@
+<template>
+  <base-container>
+    <h2>Active Users</h2>
+    <base-search
+      @search="updateSearch"
+      :search-term="enteredSearchTerm"
+    ></base-search>
+    <div>
+      <button @click="sort('asc')" :class="{ selected: sorting === 'asc' }">
+        Sort Ascending
+      </button>
+      <button @click="sort('desc')" :class="{ selected: sorting === 'desc' }">
+        Sort Descending
+      </button>
+    </div>
+    <ul>
+      <user-item
+        v-for="user in displayedUsers"
+        :key="user.id"
+        :user-name="user.fullName"
+        :id="user.id"
+        @list-projects="$emit('list-projects', $event)"
+      ></user-item>
+    </ul>
+  </base-container>
+</template>
+
+<script>
+import { ref, computed, watch } from 'vue'
+import UserItem from './UserItem.vue'
+
+export default {
+  components: {
+    UserItem
+  },
+  props: ['users'],
+  emits: ['list-projects'],
+  setup (props) {
+    const enteredSearchTerm = ref('')
+    const activeSearchTerm = ref('')
+
+    const availableUsers = computed(() => {
+      let users = []
+      if (activeSearchTerm.value) {
+        users = props.users.filter(usr =>
+          usr.fullName.includes(activeSearchTerm.value)
+        )
+      } else if (props.users) {
+        users = props.users
+      }
+      return users
+    })
+
+    watch(enteredSearchTerm, newVal => {
+      setTimeout(() => {
+        if (newVal === enteredSearchTerm.value) {
+          activeSearchTerm.value = newVal
+        }
+      }, 300)
+    })
+
+    const updateSearch = val => {
+      enteredSearchTerm.value = val
+    }
+
+    const sorting = ref(null)
+
+    const displayedUsers = computed(() => {
+      if (!sorting.value) {
+        return availableUsers.value
+      }
+      return availableUsers.value.slice().sort((u1, u2) => {
+        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
+          return 1
+        } else if (sorting.value === 'asc') {
+          return -1
+        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
+          return -1
+        } else {
+          return 1
+        }
+      })
+    })
+
+    const sort = mode => {
+      sorting.value = mode
+    }
+
+    return {
+      enteredSearchTerm,
+      updateSearch,
+      sorting,
+      displayedUsers,
+      sort
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
